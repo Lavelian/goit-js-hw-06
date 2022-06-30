e48e516872bfe7a69a01e092e35d1f71ee7d4ef6
Lavelian <daniil.solodkyi@gmail.com>
2022-06-30 10:26:11 +0300

    final#2

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const categoriesEl = document.querySelector('#categories');
+console.log('Number of categories:', categoriesEl.children.length);
+const arrayLi = [...document.querySelectorAll('h2 + ul')];
+const itemsEl = [...document.querySelectorAll('.item')];
+
+itemsEl.forEach((element, index) => {
+  console.log(
+    `Category: ${element.firstElementChild.textContent} \nElements:${arrayLi[index].children.length}`
+  );
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const ulEl = document.querySelector('#ingredients');
+ulEl.classList.add('#ul');
+console.log(ulEl);
+
+// document.createElement('li');
+
+ingredients.forEach(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = ingredient;
+  console.log(itemEl);
+  ulEl.append(itemEl);
+});
