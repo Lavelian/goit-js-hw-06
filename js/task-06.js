c827d7dffb59960bb6a12ad64b7f8b5258bc5c43
Lavelian <daniil.solodkyi@gmail.com>
2022-06-30 18:09:15 +0300

    final1111

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const inputEl = document.querySelector('input');
 console.log(inputEl.dataset.length);
 inputEl.addEventListener('blur', event => {
   if (event.currentTarget.value.length === Number(inputEl.dataset.length)) {
-    inputEl.classList.remove('valid');
+    inputEl.classList.remove('invalid');
 
     console.log('Green', inputEl);
     console.log(event.currentTarget.value.length);
