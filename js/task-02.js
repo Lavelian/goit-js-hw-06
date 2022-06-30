ceecdb20ef1cba5c9c020e647cfc54ecb00354c4
Lavelian <daniil.solodkyi@gmail.com>
2022-06-30 11:56:03 +0300

    final#3

--- js/task-02.js
+++ js/task-02.js
@@ -1,14 +1,12 @@
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
 const ulEl = document.querySelector('#ingredients');
 ulEl.classList.add('#ul');
-console.log(ulEl);
 
-// document.createElement('li');
-
-ingredients.forEach(ingredient => {
+const clone = ingredients.map(ingredient => {
   const itemEl = document.createElement('li');
   itemEl.classList.add('item');
   itemEl.textContent = ingredient;
-  console.log(itemEl);
-  ulEl.append(itemEl);
+  return itemEl;
 });
+ulEl.append(...clone);
+console.log(clone);
--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,12 @@
+// Напиши скрипт для создания галереи изображений по массиву данных. В HTML есть список ul.gallery.
+
+// <ul class="gallery"></ul>
+// Используй массив объектов images для создания элементов <img> вложенных в <li>. Для создания разметки используй шаблонные строки
+// и метод insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +21,17 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const ulEl = document.querySelector('.gallery');
+console.log(ulEl);
+
+const imgEl = document.createElement('img');
+const liEl = document.createElement('li');
+
+const arrayImg = images
+  .map(({ url, alt }) => {
+    const imgEl = document.createElement('img');
+    return `<li><img alt='${alt}'; src='${url}';></li>`;
+  })
+  .join(' ');
+
+ulEl.insertAdjacentHTML('beforeend', `${arrayImg}`);
